0a7e2d04dd49356cc14ac185979871eb2d792b04
橘子掀开夜晚 <487331044@qq.com>
2024-10-09 15:21:09 +0800

    fix

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
 import { viteBundler } from '@vuepress/bundler-vite'
-import { webpackBundler } from '@vuepress/bundler-webpack'
+// import { webpackBundler } from '@vuepress/bundler-webpack'
 import { removePwaPlugin } from '@vuepress/plugin-remove-pwa'
 
 export default defineUserConfig({
@@ -9,8 +9,20 @@ export default defineUserConfig({
   title: "Java",
   description: "编码见闻录",
   bundler: viteBundler(),
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   // bundler: webpackBundler(),
   theme: recoTheme({
+
     // style: "@vuepress-reco/style-default",
     logo: "/icon.svg",
     author: "gzc",
@@ -204,17 +216,7 @@ export default defineUserConfig({
     selectLanguageName: '简体中文',
   }),
   // debug: true,
-  // head: [
-  //   ['link', { rel: 'icon', href: '/logo.png' }],
-  //   ['link', { rel: 'manifest', href: '/manifest.json' }],
-  //   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-  //   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-  //   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-  //   ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
-  //   ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-  //   ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-  //   ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
-  // ],
+
   plugins: [
     removePwaPlugin({
       // 选项
