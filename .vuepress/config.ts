d3402eb6673f7996a94b4f4f188c166c3562ced2
橘子掀开夜晚 <487331044@qq.com>
2024-10-09 14:25:23 +0800

    pwa

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
 import { viteBundler } from '@vuepress/bundler-vite'
 import { webpackBundler } from '@vuepress/bundler-webpack'
+import { removePwaPlugin } from '@vuepress/plugin-remove-pwa'
 
 export default defineUserConfig({
   base: '/java_blog/',
@@ -203,4 +204,25 @@ export default defineUserConfig({
     selectLanguageName: '简体中文',
   }),
   // debug: true,
+  // head: [
+  //   ['link', { rel: 'icon', href: '/logo.png' }],
+  //   ['link', { rel: 'manifest', href: '/manifest.json' }],
+  //   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  //   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+  //   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+  //   ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+  //   ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+  //   ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+  //   ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  // ],
+  plugins: [
+    removePwaPlugin({
+      // 选项
+      // showInstall: true,
+      // updatePopup: {
+      //   message: "发现新内容可用",
+      //   buttonText: "刷新"
+      // }
+    }),
+  ],
 });
